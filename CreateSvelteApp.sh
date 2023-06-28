885d58a2c7f672edf6cb77145634d0d303fa6c5b
patrick11514 <ja@patrick115.eu>
2023-06-28 14:52:28 +0200

    Added tailwind example in main file

--- CreateSvelteApp.sh
+++ CreateSvelteApp.sh
@@ -88,6 +88,16 @@ EOF
 EOF
 )
             echo "$LAYOUT" > src/routes/+layout.svelte
+
+            MAIN_FILE=$(cat <<EOF
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    <span class="text-red-500">text-red-500</span>
+</p>
+EOF
+)
+            echo "$MAIN_FILE" > src/routes/+page.svelte
             break;;
         No ) break;;
     esac
