0077cef1e565bb5d7f56364562e83b79cba660d0
Patrik Mintěl <ja@patrick115.eu>
2023-10-08 01:03:19 +0200

    Renamed files

--- CreatePatrick115App/deploy.js
+++ CreatePatrick115App/deploy.js
@@ -1,8 +1,8 @@
-const fs = require("node:fs")
-const packageJson = require("./package.json")
-packageJson.main = "./CreateSvelteApp.js"
+const fs = require('node:fs')
+const packageJson = require('./package.json')
+packageJson.main = './CreatePatrick115App.js'
 packageJson.scripts = {}
 packageJson.devDependencies = {}
 
-fs.writeFileSync("./build/package.json", JSON.stringify(packageJson, null, 4))
-fs.renameSync("./build/index.js", "./build/CreateSvelteApp.js")
+fs.writeFileSync('./build/package.json', JSON.stringify(packageJson, null, 4))
+fs.renameSync('./build/index.js', './build/CreatePatrick115App.js')
