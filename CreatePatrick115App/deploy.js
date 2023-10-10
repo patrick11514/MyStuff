f5f70de00e67cc211f071fe60308b9b76e4f1919
patrick11514 <ja@patrick115.eu>
2023-10-10 21:13:08 +0200

    Moved into submodule

--- CreatePatrick115App/deploy.js
+++
@@ -1,8 +0,0 @@
-const fs = require('node:fs')
-const packageJson = require('./package.json')
-packageJson.main = './CreatePatrick115App.js'
-packageJson.scripts = {}
-packageJson.devDependencies = {}
-
-fs.writeFileSync('./build/package.json', JSON.stringify(packageJson, null, 4))
-fs.renameSync('./build/index.js', './build/CreatePatrick115App.js')
